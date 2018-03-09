b08b87b388ff4c059ab33a9e29530745c2c5964e
aciremaaleman <aciremaaleman@outlook.es>
2018-03-09 10:44:18 -0600

    Agregando ecommerce

---
+++ README.md
@@ -0,0 +1,3 @@
+## Ecommerce
+
+El reto de este proyecto fue agregar productos al local Storage y utilizar la api de Paypal para hacer pagos.
---
+++ js/data.js
@@ -0,0 +1,74 @@
+var data = {
+  products: [
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Nintendo Switch",
+      description: "Just another description",
+      id: 1,
+      price: 3900
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 4",
+      description: "Just another description",
+      id: 2,
+      price: 6900
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 3",
+      description: "Yes",
+      id: 3,
+      price: 6500
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 2",
+      description: "Yes",
+      id: 4,
+      price: 1000
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 1",
+      description: "Yes",
+      id: 5,
+      price: 9500
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Nintendo 64",
+      description: "Yes",
+      id: 6,
+      price: 700
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Sims 3",
+      description: "Great Game",
+      id: 7,
+      price: 450
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Sims 4",
+      description: "Great Game",
+      id: 8,
+      price: 1000
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "The last of us",
+      description: "You need to play it",
+      id: 9,
+      price: 700
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Uncharted",
+      description: "Yes",
+      id: 10,
+      price: 800
+    }
+  ]
+};
